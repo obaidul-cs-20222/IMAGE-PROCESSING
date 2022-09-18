68f106b386328808e4d54795bf6ab0f72262b380
obaidul <mdobaidulislam786@outlook.com>
2022-09-18 21:24:16 +0530

    another commit

--- intro.py
+++ intro.py
@@ -1,7 +1,7 @@
 import cv2
-
 im=cv2.imread('dd.jpg')
 cv2.imshow('imagedisplay',im)
+print("shape of the image is",end=" ")
 print(im.shape)
 h,w,c=im.shape
 print(h,w,c)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import cv2 
-import time
+
 im=cv2.imread('6.JPG')
 cv2.imshow('imq',im)
-time.sleep(5)
+cv2.waitKey(0)
 
